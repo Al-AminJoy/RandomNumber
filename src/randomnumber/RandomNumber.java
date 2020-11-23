7dc9e8cd132327312cf99350b76c24889a5369e7
Al-AminJoy <alaminislam3555@gmail.com>
2020-11-23 21:56:34 +0600

    Complete

---
+++ src/randomnumber/RandomNumber.java
@@ -0,0 +1,14 @@
+package randomnumber;
+
+import java.util.Random;
+
+public class RandomNumber {
+
+    public static void main(String[] args) {
+       Random rand=new Random();
+       int randomNum=rand.nextInt(10);
+       
+       System.out.println(randomNum);
+    }
+    
+}
